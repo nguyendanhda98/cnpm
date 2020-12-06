6d75d4116792eed66c6dcc6d008a14c2ddafe400
Dand <nguyendanhda98@gmail.com>
2020-12-06 18:10:04 +0700

    QuanLyPhong

--- src/components/QuanLyPhong.js
+++ src/components/QuanLyPhong.js
@@ -17,17 +17,24 @@ class QuanLyPhong extends React.Component {
         this.setState({ phongs: "Hiện tại chưa có phòng ở đây" });
       } else {
         const z = x.map((y) => {
+          let khach = y.emailkhach;
+          if (khach === "") {
+            khach = "Ẩn";
+          } else {
+            khach = "Hiện";
+          }
           return (
             <div key={y._id}>
               <Phong phong={y} />
-              <button
+              <input
                 id={y._id}
                 className="btn btn-primary"
                 name={y._id}
                 onClick={this.anhien}
-              >
-                Ẩn
-              </button>
+                type="submit"
+                value={khach}
+              />
+
               <button id={y._id} className="btn btn-primary" onClick={this.xoa}>
                 Xoá
               </button>
@@ -51,42 +58,31 @@ class QuanLyPhong extends React.Component {
 
   anhien = (e) => {
     let id = e.target.id;
+    let val = document.getElementById(id).value;
     let url = `http://localhost:3030/phong/${id}`;
     let data = { emailkhach: "an" };
+    if (val === "Ẩn") {
+      document.getElementById(id).value = "Hiện";
+      data = { emailkhach: "an" };
+    } else {
+      document.getElementById(id).value = "Ẩn";
+      data = { emailkhach: "" };
+    }
     axios.patch(url, data);
-    let val = document.getElementsByName(id).value;
-    console.log(val);
-    
-  }
+  };
+
   xoa = (e) => {
     let id = e.target.id;
     let url = `http://localhost:3030/phong/${id}`;
     axios.delete(url);
     alert("Xoá thành công");
     window.location.replace("/QuanLyPhong");
-  }
+  };
 
   render() {
     return (
-      <div>{this.state.phong}</div>
-      // <div>
-      //   <div className="card" style={{ width: "18rem" }}>
-      //     <img src="../logo192.png" className="card-img-top" alt="..." />
-      //     <div className="card-body">
-      //       <h5 className="card-title">Cầu Giấy</h5>
-      //       <p className="card-text">1,2 triệu/tháng</p>
-      //       <a href="/chitiet" className="btn btn-primary">
-      //         Chi tiết
-      //       </a>
-      //       <a href="/chitiet" className="btn btn-primary">
-      //         Ẩn
-      //       </a>
-      //       <a href="/chitiet" className="btn btn-primary">
-      //         Xoá
-      //       </a>
-      //     </div>
-      //   </div>
-      // </div>
+      // phong
+      <div className="row">{this.state.phong}</div>
     );
   }
 }
