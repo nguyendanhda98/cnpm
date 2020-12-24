14a5380f1570d725f5b948665d86085c34ea20a1
Dand Nguyen <nguyendanhda98@gmail.com>
2020-12-25 03:33:20 +0700

    Appbar

--- src/components-remake/AppBar.jsx
+++ src/components-remake/AppBar.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,20 +18,22 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const dangnhap = (e) => {
-  console.log(e);
-  //window.location.replace("/dangnhap");
-};
+
+
+export default function ButtonAppBar() {
 
 const dangxuat = (e) => {
   e.preventDefault();
   alert("Đăng xuất thành công");
+  setTen("")
+  setLoai("")
   localStorage.removeItem("email");
   localStorage.removeItem("ten");
+  localStorage.removeItem("loai");
+
   window.location.replace("/");
 };
 
-export default function ButtonAppBar() {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
 
@@ -38,18 +41,91 @@ export default function ButtonAppBar() {
     setValue(newValue);
   };
 
+  const [Ten, setTen] = useState(localStorage.getItem("ten"));
+  const [Loai, setLoai] = useState(localStorage.getItem("loai"));
+
   return (
     <div className={classes.root}>
       <AppBar position="static">
-        
         <Tabs
           value={value}
           onChange={handleChange}
           aria-label="simple tabs example"
         >
-          <Tab label="Trang chủ" component={Link} to="/" />
-          <Tab label="Đăng nhập" component={Link} to="/dangnhap" />
-          <Tab label="Đăng ký" component={Link} to="/dangky" />
+          <Tab
+            label="Hệ thống thuê và cho thuê phòng trọ TDT"
+            component={Link}
+            to="/"
+          />
+          <Tab
+            style={{ display: Ten ? "none" : "block" }}
+            label="Đăng nhập"
+            component={Link}
+            to="/dangnhap"
+          />
+          <Tab
+            style={{ display: Ten ? "none" : "block" }}
+            label="Đăng ký"
+            component={Link}
+            to="/dangky"
+          />
+          <Tab
+            style={{
+              display: Loai === "chunha" ? "block" : "none",
+            }}
+            label="Cho thuê phòng"
+            component={Link}
+            to="/chunha"
+          />
+          <Tab
+            style={{ display: Ten ? "block" : "none" }}
+            label="Thông tin cá nhân"
+            component={Link}
+            to="/ThongTinCaNhan"
+          />
+          <Tab
+            style={{ display: Ten ? "block" : "none" }}
+            label="Phòng đã yêu cầu"
+            component={Link}
+            to="/PhongDaDat"
+          />
+          <Tab
+            style={{
+              display: Loai === "chunha" ? "block" : "none",
+            }}
+            label="Phòng đã cho thuê"
+            component={Link}
+            to="/PhongDaChoThue"
+          />
+          <Tab
+            style={{
+              display: Loai === "chunha" ? "block" : "none",
+            }}
+            label="Yêu cầu đặt phòng"
+            component={Link}
+            to="/YeuCauDatPhong"
+          />
+          <Tab
+            style={{
+              display: Loai === "chunha" ? "block" : "none",
+            }}
+            label="Quản lý phòng"
+            component={Link}
+            to="/QuanLyPhong"
+          />
+          <Tab
+            style={{ display: Ten ? "block" : "none" }}
+            label="Đăng Xuất"
+            component={Link}
+            to="#"
+            onClick={dangxuat}
+          />
+          <Typography
+            style={{ display: Ten ? "block" : "none" }}
+            component="div"
+          >
+            Xin chào {Ten}
+          </Typography>
         </Tabs>
       </AppBar>
     </div>
